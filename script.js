1b78f4382a3a2d2e11926f18bc875e8e8ad2b5c9
riccisilva-coder <ricci.silva@escola.pr.gov.br>
2026-09-14 11:27:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const botoes = document.querySelectorAll(button);
 
+botoes.forEach(function (botao) {
+  let curtiu = false;
+  botao.addEventListener("click", botaoClicado);
+  function botaoClicado() {
+    console.log("fui clicado");
+    let txto = botao.querySelector("span");
+    if (curtiu == false) {
+      texto.textContent++;
+      curtiu = true;
+    } else{
+      text.textContent --;
+      curtiu = false;
+    }
+  }
+}
